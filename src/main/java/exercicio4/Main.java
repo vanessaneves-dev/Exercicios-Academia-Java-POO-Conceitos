b4978502929c257150d11e4edfedab32f7e0f7cf
Vanessa Neves <113128675+vanessaneves-dev@users.noreply.github.com>
2024-06-06 19:21:40 -0300

    :sparkles: feat: Exercicio 4 entegue

---
+++ src/main/java/exercicio4/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // Criar instâncias de Paciente com valores diferentes
+        Paciente paciente1 = new Paciente(76.0, 1.64);
+        paciente1.setNome("Larissa");
+
+        Paciente paciente2 = new Paciente(120.0, 1.90);
+        paciente2.setNome("Rodrigo");
+
+        Paciente paciente3 = new Paciente(60.0, 1.60);
+        paciente3.setNome("Isabella.");
+
+        // Exibir o resultado dos métodos criados no console
+        System.out.println(paciente1);
+        System.out.println(paciente2);
+        System.out.println(paciente3);
+    }
+}
